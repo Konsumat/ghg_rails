e8f512517f9ae80dea990b2d2e0f02831680db72
Konsumat <30664856+Konsumat@users.noreply.github.com>
2017-10-08 19:31:33 +0200

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
+
+  # GET /pages
+  # GET /pages.json
+  def index
+    sql = "select luc_nr, submission, pool_abbr, inventory_year, substance, unit, value from pbeissert.em_annu limit 10"
+    @records_array = ActiveRecord::Base.connection.execute(sql)
+  end
+end
