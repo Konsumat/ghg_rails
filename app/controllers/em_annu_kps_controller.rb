8d8d250cd15e33cf6a0ed9f727510020ec5837cf
Konsumat <30664856+Konsumat@users.noreply.github.com>
2017-11-05 16:07:03 +0100

    deleted unnecessary stuff, fixed csv, added charts to em_annu_kps

--- app/controllers/em_annu_kps_controller.rb
+++ app/controllers/em_annu_kps_controller.rb
@@ -1,14 +1,40 @@
 class EmAnnuKpsController < ApplicationController
   before_action :set_page, only: %i[show edit update destroy]
 
-  # GET /pages
-  # GET /pages.json
   def index
-    @luc_nrs = EmAnnuKp.all_luc_nrs
-    @pool_abbrs = EmAnnuKp.all_pool_abbrs
+    @luc_names = EmAnnuKp.all_luc_names
+    @pool_name_ens = EmAnnuKp.all_pool_name_ens
     @substances = EmAnnuKp.all_substances
     @submissions = EmAnnuKp.all_submissions
-    @q = EmAnnuKp.order('inventory_year').ransack(params[:q])
-    @em_annu_kps = @q.result(distinct: true).paginate(page: params[:page])
+    
+    @q = EmAnnuKp.ransack(params[:q])
+    @q.luc_name_eq = @luc_names.first unless params[:q]
+    @q.submission_eq = @submissions.first unless params[:q]
+    @results = @q.result(distinct: true)
+    @chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: 'Time Series Pools')
+      f.xAxis(categories: @results.order('inventory_year').map(&:inventory_year).uniq.sort!)
+      f.legend(layout: 'horizontal', align: 'center')
+      all_series = @results.order('inventory_year desc').group_by { |x| x['pool_name_en'] }.map(&:last)
+      all_series.each do |series|
+        values = series.map(&:value)
+        rounded_values = []
+        values.each do |value|
+          rounded_values << value.to_f.round(2)
+        end
+        f.series(name: series.first['pool_name_en'], data: rounded_values)
+      end
+      f.chart({renderTo: "chart1"})
+      f.plot_options(column: { stacking: 'normal' })
+
+      f.yAxis [
+        { title: { text: 'kt Co2-eq', margin: 70 } }
+      ]
+      f.chart(defaultSeriesType: 'column')
+    end
+    respond_to do |format|
+      format.html
+      format.csv { send_data EmAnnuKp.export(@results), type: 'text/csv; charset=utf-8; header=present', disposition: "attachment; filename=export.csv" }
+    end
   end
 end
