01f1f3fccb17958e1c7e898824a88accd5a35399
Konsumat <30664856+Konsumat@users.noreply.github.com>
2017-11-06 11:45:46 +0100

    dynamic title added to chart in em_ttt_ghg_lulucfs --- but couldnt make it work with two variables because I am stupid

--- app/controllers/em_ttt_ghg_lulucfs_controller.rb
+++ app/controllers/em_ttt_ghg_lulucfs_controller.rb
@@ -10,7 +10,7 @@ class EmTttGhgLulucfsController < ApplicationController
     @q.submission_eq = @submissions.first unless params[:q]
     @results = @q.result(distinct: true)
     @chart = LazyHighCharts::HighChart.new('graph') do |f|
-      f.title(text: 'Time Series Emissions')
+      f.title(text: @results.order('luc_name').map(&:luc_name).uniq.sort!)
       f.xAxis(categories: @results.order('inventory_year').map(&:inventory_year).uniq.sort!)
       f.legend(layout: 'horizontal', align: 'center')
       all_series = @results.order('inventory_year').group_by { |x| x['substance'] }.map(&:last)
